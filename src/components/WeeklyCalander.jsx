a194d60003e5c4d27331c0a972068c6794629216
Vangjel <vfrasheri21@gmail.com>
2024-02-06 14:40:05 -0500

    removed commented out code

--- src/components/WeeklyCalander.jsx
+++ src/components/WeeklyCalander.jsx
@@ -46,13 +46,6 @@ const WeeklyCalander = (props) => {
                 )
             }
 
-{/* <CalenderEntry day={"Sunday"} lessonTime={"7:00 PM"} isOutdoor={true}/>
-<CalenderEntry day={"Monday"} lessonTime={"7:00 PM"} isOutdoor={false}/>
-<CalenderEntry day={"Tuesday"} lessonTime={"7:00 PM"} isOutdoor={true}/>
-<CalenderEntry day={"Wednesday"} lessonTime={"7:00 PM"} isOutdoor={true}/>
-<CalenderEntry day={"Thursday"} lessonTime={"7:00 PM"} isOutdoor={false}/>
-<CalenderEntry day={"Friday"} lessonTime={"7:00 PM"} isOutdoor={true}/>
-<CalenderEntry day={"Saturday"} lessonTime={"7:00 PM"} isOutdoor={true}/> */}
             
         </div>
 
